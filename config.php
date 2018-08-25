00142c8e8ab91edd73cd5ad78a65be9abf8a0996
fyzarya <42697633+fyzarya@users.noreply.github.com>
2018-08-25 21:27:18 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+##############################################################################################################
+$config['urls'] = 'hUDJSNChdiej73dosN8S7KcyJneEdcrypto2'; //from urls
+$config['sessid'] = '8s5tjmtkdksj2j46ojh1b0pf12'; //from http cookie PHPSESSID headers
+$config['worker'] = '50'; //choose wisely, be careful, too much worker causing banned account
+##############################################################################################################
